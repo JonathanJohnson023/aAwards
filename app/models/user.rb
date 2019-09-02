c75d4650b4f55b89c51fb2cd3ac548e234d44674
Jonathan <jonathanjohnson023@gmail.com>
2019-09-02 12:29:34 -1000

    User Model auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,29 @@ class User < ApplicationRecord
 
   after_initialize :ensure_session_token
 
-  
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_password?(password) ? user : nil
+  end
 
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+ end
 
+ def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+ end
+
+ def reset_session_token
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+ end
+
+ def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+ end
+
+ 
 end
